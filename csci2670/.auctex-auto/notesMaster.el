b577731466c6c22db956959a358f8355ee7e288c
sudhan.chitgopkar <sudhanc03@yahoo.com>
2021-01-14 14:01:01 -0500

    added day 1 notes

---
+++ csci2670/.auctex-auto/notesMaster.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "notesMaster"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org2dcc34e"
+    "sec:org7d578eb"
+    "sec:orgd89327e"
+    "sec:org9cceb3d"
+    "sec:org1a9b58d"
+    "sec:org440ec9e"
+    "sec:orga09a867"
+    "sec:orgb285053"
+    "sec:org3d64faf"))
+ :latex)
+
