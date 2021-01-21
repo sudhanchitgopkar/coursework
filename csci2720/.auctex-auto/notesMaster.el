ff314d0fbe16d96ebb7203675727cf37b22401cf
sudhan.chitgopkar <sudhanc03@yahoo.com>
2021-01-21 12:49:10 -0500

    final added 2720 notes

--- csci2720/.auctex-auto/notesMaster.el
+++ csci2720/.auctex-auto/notesMaster.el
@@ -5,13 +5,14 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,11 +32,16 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgcda033b"
-    "sec:orgaed776a"
-    "sec:orgdd05fb0"
-    "sec:orgc248836"
-    "sec:org529f060"
-    "sec:orgb2b6948"))
+    "sec:org7c2c59f"
+    "sec:org2f33709"
+    "sec:org4b9017d"
+    "sec:org0bc9c88"
+    "sec:org89fa4d3"
+    "sec:orgaf2cfd5"
+    "sec:org3b3000d"
+    "sec:orgf272556"
+    "sec:org26d4733"
+    "sec:org6c27adb"
+    "sec:org607754f"))
  :latex)
 
