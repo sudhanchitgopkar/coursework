058df4de8b725a9af849a73186c0b225103d9d69
sudhan.chitgopkar <sudhanc03@yahoo.com>
2021-01-24 17:45:04 -0500

    added mod 1 survey, reader notes

---
+++ intl4710/.auctex-auto/notesMaster.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "notesMaster"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orge3fae22"
+    "sec:org70fa73e"
+    "sec:orgf1a6659"
+    "sec:orgbaa1d3f"
+    "sec:org0ecf25b"
+    "sec:org333b3ca"
+    "sec:orgddc2b35"
+    "sec:org825b2b9"
+    "sec:org3eddb61"))
+ :latex)
+
