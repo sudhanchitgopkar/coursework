56c2584cca9b3825d3c91524ccef2c7d6d677cec
sudhan.chitgopkar <sudhanc03@yahoo.com>
2021-01-27 10:46:01 -0500

    4260 update 1, 01.27.20

--- intl4260/.auctex-auto/notesMaster.el
+++ intl4260/.auctex-auto/notesMaster.el
@@ -4,14 +4,14 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=1in")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -29,36 +29,42 @@
     "textcomp"
     "amssymb"
     "capt-of"
-    "hyperref")
+    "hyperref"
+    "geometry")
    (LaTeX-add-labels
-    "sec:org6f2b152"
-    "sec:org5890f10"
-    "sec:org5e1a6ae"
-    "sec:org5ba1fda"
-    "sec:orgbe65f8c"
-    "sec:org383b91c"
-    "sec:org9238659"
-    "sec:orgde76b61"
-    "sec:orgdaf4177"
-    "sec:org30f4974"
-    "sec:org2c9e6a1"
-    "sec:org7055181"
-    "sec:orge3645f2"
-    "sec:orgbb5ae44"
-    "sec:orge3d73fb"
-    "sec:org1c19409"
-    "sec:org61a1c5c"
-    "sec:org7f5b4c0"
-    "sec:org7e123bb"
-    "sec:orge70ceb0"
-    "sec:org4735795"
-    "sec:org612b195"
-    "sec:orgfdad87d"
-    "sec:orgc18705d"
-    "sec:orga032389"
-    "sec:org60349a6"
-    "sec:org165695f"
-    "sec:org16f4810"
-    "sec:orgf66753e"))
+    "sec:org5746417"
+    "sec:org59b2e85"
+    "sec:orgaed84ab"
+    "sec:org58c2e24"
+    "sec:org78d3a77"
+    "sec:org4c00156"
+    "sec:org9b5da50"
+    "sec:orgc6e09ca"
+    "sec:org9c12480"
+    "sec:org4ce23a0"
+    "sec:orgcf4aacc"
+    "sec:org79ebc14"
+    "sec:org1a94732"
+    "sec:orgf52bfca"
+    "sec:orgfc00d4c"
+    "sec:orga8554e5"
+    "sec:orgeed819d"
+    "sec:orgaef96c2"
+    "sec:orgb7ad75f"
+    "sec:org6693945"
+    "sec:org3d82c2c"
+    "sec:org35592f4"
+    "sec:org321b63c"
+    "sec:orged7b7b6"
+    "sec:org12eee33"
+    "sec:orgc37bf91"
+    "sec:org9d7e8b8"
+    "sec:orga9a3fc3"
+    "sec:orgdad0de1"
+    "sec:org31fd746"
+    "sec:orgafbba29"
+    "sec:orgfc328f2"
+    "sec:org5b15b93"
+    "sec:org806a146"))
  :latex)
 
