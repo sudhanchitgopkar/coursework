177b073554ab359fd7411e0c612a9008884306d7
sudhan.chitgopkar <sudhanc03@yahoo.com>
2021-01-28 17:48:38 -0500

    final 2670 notes:

--- .auctex-auto/csciSend.el
+++ .auctex-auto/csciSend.el
@@ -1,5 +1,5 @@
 (TeX-add-style-hook
- "notesMaster"
+ "csciSend"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
@@ -32,39 +32,11 @@
     "hyperref"
     "geometry")
    (LaTeX-add-labels
-    "sec:org7880746"
-    "sec:org1b57d0b"
-    "sec:org5bad897"
-    "sec:orgaf6f310"
-    "sec:orga0827d5"
-    "sec:org9dd4d5c"
-    "sec:orgd3dde2b"
-    "sec:org94a2565"
-    "sec:orgeb4f6ce"
-    "sec:org1c18134"
-    "sec:org56ffdc1"
-    "sec:org8a01f2a"
-    "sec:org7c29eb2"
-    "sec:orgd5dd2da"
-    "sec:org28ee869"
-    "sec:org3b9dad3"
-    "sec:orgb9441aa"
-    "sec:orgf9ddae8"
-    "sec:org1cc997a"
-    "sec:orgca4c934"
-    "sec:orgfd6149f"
-    "sec:org6cd3f36"
-    "sec:org2212daa"
-    "sec:org7c231af"
-    "sec:orgc15494f"
-    "sec:orgf7e08fe"
-    "sec:org085ee65"
-    "sec:orgf21f6fd"
-    "sec:orgda89913"
-    "sec:org43304f5"
-    "sec:orgfc3534b"
-    "sec:org858b2fd"
-    "sec:org25d566d"
-    "sec:orge1149e3"))
+    "sec:orgb197025"
+    "sec:org159b1f9"
+    "sec:orgc13fef1"
+    "sec:org4cb47f5"
+    "sec:orge59b226"
+    "sec:org7f85c5c"))
  :latex)
 
